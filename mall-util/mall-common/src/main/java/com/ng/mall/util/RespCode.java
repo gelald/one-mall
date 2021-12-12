0115955629a02ce5fb0f4143c8fe6bf0232d0af1
wuyingbin <yb.ng@foxmail.com>
2021-12-12 18:24:00 +0800

    完善结果响应类

--- mall-util/mall-common/src/main/java/com/ng/mall/util/RespCode.java
+++ mall-util/mall-common/src/main/java/com/ng/mall/util/RespCode.java
@@ -12,8 +12,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum RespCode {
+    /**
+     * 操作成功
+     */
     SUCCESS(2000, "操作成功"),
+    /**
+     * 操作失败
+     */
     FAILED(5000, "操作失败"),
+    /**
+     * 服务器异常
+     */
     ERROR(5001, "系统异常");
 
     private Integer code;
--- mall-util/mall-common/src/main/java/com/ng/mall/util/RespResult.java
+++ mall-util/mall-common/src/main/java/com/ng/mall/util/RespResult.java
@@ -1,5 +1,6 @@
 package com.ng.mall.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,8 +14,59 @@ import java.io.Serializable;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RespResult<T> implements Serializable {
+    /**
+     * 响应数据
+     */
     private T data;
+    /**
+     * 响应状态码
+     *
+     * @see RespCode
+     */
     private Integer code;
+    /**
+     * 响应信息
+     */
     private String message;
+
+    public RespResult(RespCode resultCode) {
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+    }
+
+    public RespResult(T data, RespCode resultCode) {
+        this.data = data;
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+    }
+
+    public static RespResult<?> ok() {
+        return new RespResult<>(null, RespCode.SUCCESS);
+    }
+
+    public static <T> RespResult<T> ok(T data) {
+        return new RespResult<>(data, RespCode.SUCCESS);
+    }
+
+    public static RespResult<?> error() {
+        return new RespResult<>(null, RespCode.ERROR);
+    }
+
+    public static RespResult<?> error(RespCode resultCode) {
+        return new RespResult<>(resultCode);
+    }
+
+    public static RespResult<?> error(String message) {
+        return secByError(RespCode.ERROR.getCode(), message);
+    }
+
+    //自定义异常
+    public static RespResult<?> secByError(Integer code, String message) {
+        RespResult<?> err = new RespResult<>();
+        err.setCode(code);
+        err.setMessage(message);
+        return err;
+    }
 }
